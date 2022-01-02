77e8443db0e2821954e120095d050f3fbcc1a116
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2022-01-02 14:28:37 +0900

    予定登録機能の実装（保存不可）

--- js/event.js
+++ js/event.js
@@ -1,22 +1,28 @@
 var eventList  = [];
 var demoEvent = { year:2022, month:1, day:4, title:"課題提出"};
-eventList.push(demoEvent)
-console.log(eventList);
+var demoEvent2 = { year:2022, month:1, day:1, title:"お正月"};
+var demoEvent3 = { year:2022, month:1, day:2, title:"20時からアルバイト"};
+eventList.push(demoEvent);
+eventList.push(demoEvent2);
+eventList.push(demoEvent3);
 
 
 function showEvent(id){
     var year = id.substr(0, 4);
     var month = id.substr(4, 2);
     var day = id.substr(6, 2);
-    console.log(year, month, day);
     if(isEvent(year, month-1, day)){
         document.getElementById(id).style.visibility = "visible";
     }
-    
 }
 
 function hideEvent(id){
-    document.getElementById(id).style.visibility = "hidden";
+    var year = id.substr(0, 4);
+    var month = id.substr(4, 2);
+    var day = id.substr(6, 2);
+    if(isEvent(year, month-1, day)){
+        document.getElementById(id).style.visibility = "hidden";
+    }
 }
 
 function isEvent(year, month, day){
@@ -26,6 +32,22 @@ function isEvent(year, month, day){
         }
     }
     return false;
+}
+
+function addEvent(){
+    var eventTitle =  document.getElementById("eventTitle").value;
+    var eventDate = document.getElementById("eventDate").value;
+    var eventYear = parseInt(eventDate.substr(0, 4));
+    var eventMonth = parseInt(eventDate.substr(5, 2));
+    var eventDay = parseInt(eventDate.substr(8, 2));
+    if(isEvent(eventYear, eventMonth-1, eventDay)){
+        alert("予定を登録できるのは1日あたり1件です。");
+    }else{
+        var newEvent = { year:eventYear, month:eventMonth, day:eventDay, title:eventTitle};
+        eventList.push(newEvent);
+    }
+    document.getElementById("eventTitle").value = "";
+    showCalendar(currentMonth, currentYear);
 }
 
 function setEventId(year, month, day){
